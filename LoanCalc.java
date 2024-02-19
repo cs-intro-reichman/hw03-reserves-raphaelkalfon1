cb0b2dd284b35c31bdffbbd7607ffb0e2d89705e
raphaelkalfon1 <154341557+raphaelkalfon1@users.noreply.github.com>
2024-02-19 16:13:30 +0200

    Update LoanCalc.java

--- LoanCalc.java
+++ LoanCalc.java
@@ -1,6 +1,3 @@
-/**
-* Computes the periodical payment necessary to re-pay a given loan.
-*/
 public class LoanCalc {
 	
 	static double epsilon = 0.001;  // The computation tolerance (estimation error)
@@ -39,8 +36,13 @@ public class LoanCalc {
 	*/
 	// Side effect: modifies the class variable iterationCounter.
     public static double bruteForceSolver(double loan, double rate, int n, double epsilon) {  
-    	// Replace the following statement with your code
-    	return 0;
+		double payment = loan/n;
+	
+		while (endBalance(loan, rate, n, payment) > 0) {
+			payment = payment + epsilon;
+			iterationCounter++;
+		}
+		return payment;
     }
     
     /**
@@ -51,16 +53,34 @@ public class LoanCalc {
 	*/
 	// Side effect: modifies the class variable iterationCounter.
     public static double bisectionSolver(double loan, double rate, int n, double epsilon) {  
-    	// Replace the following statement with your code
-    	return 0;
+		double L = 0;
+    double H = loan;
+    double g = (L + H) / 2;
+
+    iterationCounter = 0; // Reset iteration counter
+
+    while ((H - L) > epsilon) {
+        if (endBalance(loan, rate, n, g) * endBalance(loan, rate, n, L) > 0) {
+            L = g;
+        } else {
+            H = g;
+        }
+        g = (L + H) / 2;
+        iterationCounter++;
     }
+
+    return g;
+}
 	
 	/**
 	* Computes the ending balance of a loan, given the sum of the loan, the periodical
 	* interest rate (as a percentage), the number of periods (n), and the periodical payment.
 	*/
 	private static double endBalance(double loan, double rate, int n, double payment) {
+		for (int i = 0; i < n; i++){
+			loan = (loan - payment) * (1+ (rate/100));
+		}
 		// Replace the following statement with your code
-    	return 0;
+    	return loan;
 	}
 }
